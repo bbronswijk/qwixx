7d85d2d986bd1b600b1806c829c21aa2879f84cb
bbronswijk <brambronswijk@gmail.com>
2024-05-15 22:14:17 +0200

    Fix sizing of images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,12 @@ import { ChevronRightIcon } from '@/ui/icons';
 
 export default function Home() {
   return (
-    <main className="h-full w-full flex items-center flex-col md:flex-row justify-center gap-8 px-8">
+    <main className="md:h-full w-full flex items-center flex-col md:flex-row justify-center gap-8 p-8">
       <Link className="border rounded-xl hover:shadow-xl duration-300 overflow-hidden" href="/default">
         <h1 className="text-slate-400 pt-6 text-center leading-none">Variant</h1>
         <h1 className="font-bold text-lg text-center mb-2 block">Standaard</h1>
-        <div className="h-36 lg:h-80 mb-4 flex items-center justify-center">
-          <Image src="/default.png" alt="default variant" width={600} height={271}/>
-        </div>
+        <Image src="/default.png" className="h-36 lg:h-80 mb-4 flex items-center justify-center object-contain"
+               alt="default variant" width={600} height={271}/>
         <button className="bg-slate-200 py-2 pl-4 pr-2 font-bold w-full flex items-center">
           Start
           <ChevronRightIcon className="ml-auto h-6 w-6 text-muted"/>
@@ -20,9 +19,8 @@ export default function Home() {
       <Link className="border rounded-xl hover:shadow-xl duration-300 overflow-hidden" href="/variant-a">
         <h1 className="text-slate-400 pt-6 text-center leading-none">Variant</h1>
         <h1 className="font-bold text-lg md:text-xl text-center block">Bonus variant A</h1>
-        <div className="h-36 lg:h-80 mb-4 flex items-center justify-center">
-          <Image src="/variant-a.png" alt="Bonus variant A" width={600} height={313}/>
-        </div>
+        <Image src="/variant-a.png" className="h-36 lg:h-80 mb-4 flex items-center justify-center object-contain"
+               alt="Bonus variant A" width={600} height={313}/>
         <button className="bg-slate-200 py-2 pl-4 pr-2 font-bold w-full flex items-center">
           Start
           <ChevronRightIcon className="ml-auto h-6 w-6 text-muted"/>
@@ -32,9 +30,8 @@ export default function Home() {
       <Link className="border rounded-xl hover:shadow-xl duration-300 overflow-hidden" href="/variant-b">
         <h1 className="text-slate-400 pt-6 text-center leading-none">Variant</h1>
         <h1 className="font-bold text-lg md:text-xl text-center">Bonus variant B</h1>
-        <div className="h-36 lg:h-80 mb-4 flex items-center justify-center">
-          <Image src="/variant-b.png" alt="default variant" width={600} height={313}/>
-        </div>
+        <Image src="/variant-b.png" className="h-36 lg:h-80 mb-4 flex items-center justify-center object-contain"
+               alt="default variant" width={600} height={313}/>
         <button className="bg-slate-200 py-2 pl-4 pr-2 font-bold w-full flex items-center">
           Start
           <ChevronRightIcon className="ml-auto h-6 w-6 text-muted"/>
