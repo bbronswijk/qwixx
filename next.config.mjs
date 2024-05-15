afb3760acc3ab22042b230dbda4297ef1b824c53
bbronswijk <brambronswijk@gmail.com>
2024-05-15 17:07:05 +0200

    Setup app as pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+import withPWA from 'next-pwa';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+// Configuration object tells the next-pwa plugin
+const withPWAConfig = withPWA({
+    dest: 'public', // Destination directory for the PWA files
+    disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode
+    register: true, // Register the PWA service worker
+    skipWaiting: true, // Skip waiting for service worker activation
+});
+
+export default withPWAConfig(nextConfig);
