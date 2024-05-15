aec1ce7feeb36d752b52d5679dec0efeae5ff91f
bbronswijk <brambronswijk@gmail.com>
2024-05-15 22:06:11 +0200

    Fix styling of cards on small screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ import { ChevronRightIcon } from '@/ui/icons';
 
 export default function Home() {
   return (
-    <main className="h-full w-full flex items-center justify-center gap-8 p-8">
+    <main className="h-full w-full flex items-center justify-center gap-8 px-8">
       <Link className="border rounded-xl hover:shadow-xl duration-300 overflow-hidden" href="/default">
         <h1 className="text-slate-400 pt-6 text-center leading-none">Variant</h1>
-        <h1 className="font-bold text-lg text-center">Standaard</h1>
-        <div className="h-32 lg:h-80 mb-4 flex items-center justify-center">
+        <h1 className="font-bold text-lg text-center mb-2 block">Standaard</h1>
+        <div className="h-36 lg:h-80 mb-4 flex items-center justify-center">
           <Image src="/default.png" alt="default variant" width={600} height={271}/>
         </div>
         <button className="bg-slate-200 py-2 pl-4 pr-2 font-bold w-full flex items-center">
@@ -19,8 +19,8 @@ export default function Home() {
 
       <Link className="border rounded-xl hover:shadow-xl duration-300 overflow-hidden" href="/variant-a">
         <h1 className="text-slate-400 pt-6 text-center leading-none">Variant</h1>
-        <h1 className="font-bold text-lg md:text-xl text-center">Bonus variant A</h1>
-        <div className="h-32 lg:h-80 mb-4 flex items-center justify-center">
+        <h1 className="font-bold text-lg md:text-xl text-center block">Bonus variant A</h1>
+        <div className="h-36 lg:h-80 mb-4 flex items-center justify-center">
           <Image src="/variant-a.png" alt="Bonus variant A" width={600} height={313}/>
         </div>
         <button className="bg-slate-200 py-2 pl-4 pr-2 font-bold w-full flex items-center">
@@ -32,7 +32,7 @@ export default function Home() {
       <Link className="border rounded-xl hover:shadow-xl duration-300 overflow-hidden" href="/variant-b">
         <h1 className="text-slate-400 pt-6 text-center leading-none">Variant</h1>
         <h1 className="font-bold text-lg md:text-xl text-center">Bonus variant B</h1>
-        <div className="h-32 lg:h-80 mb-4 flex items-center justify-center">
+        <div className="h-36 lg:h-80 mb-4 flex items-center justify-center">
           <Image src="/variant-b.png" alt="default variant" width={600} height={313}/>
         </div>
         <button className="bg-slate-200 py-2 pl-4 pr-2 font-bold w-full flex items-center">
