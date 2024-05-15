67120d2405b3740c41b6f7ed716883bb462c45a1
bbronswijk <brambronswijk@gmail.com>
2024-05-15 18:28:48 +0200

    Reduce card height on small screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <Link className="border rounded-xl hover:shadow-xl duration-300 overflow-hidden" href="/default">
         <h1 className="text-slate-400 pt-6 text-center leading-none">Variant</h1>
         <h1 className="font-bold text-lg text-center">Standaard</h1>
-        <div className="h-40 md:h-80 mb-8 flex items-center justify-center">
+        <div className="h-32 lg:h-80 mb-4 flex items-center justify-center">
           <Image src="/default.png" alt="default variant" width={600} height={271}/>
         </div>
         <button className="bg-slate-200 py-2 pl-4 pr-2 font-bold w-full flex items-center">
@@ -20,7 +20,7 @@ export default function Home() {
       <Link className="border rounded-xl hover:shadow-xl duration-300 overflow-hidden" href="/variant-a">
         <h1 className="text-slate-400 pt-6 text-center leading-none">Variant</h1>
         <h1 className="font-bold text-lg md:text-xl text-center">Bonus variant A</h1>
-        <div className="h-40 md:h-80 mb-8 flex items-center justify-center">
+        <div className="h-32 lg:h-80 mb-4 flex items-center justify-center">
           <Image src="/variant-a.png" alt="Bonus variant A" width={600} height={313}/>
         </div>
         <button className="bg-slate-200 py-2 pl-4 pr-2 font-bold w-full flex items-center">
@@ -32,7 +32,7 @@ export default function Home() {
       <Link className="border rounded-xl hover:shadow-xl duration-300 overflow-hidden" href="/variant-b">
         <h1 className="text-slate-400 pt-6 text-center leading-none">Variant</h1>
         <h1 className="font-bold text-lg md:text-xl text-center">Bonus variant B</h1>
-        <div className="h-40 md:h-80 mb-8 flex items-center justify-center">
+        <div className="h-32 lg:h-80 mb-4 flex items-center justify-center">
           <Image src="/variant-b.png" alt="default variant" width={600} height={313}/>
         </div>
         <button className="bg-slate-200 py-2 pl-4 pr-2 font-bold w-full flex items-center">
