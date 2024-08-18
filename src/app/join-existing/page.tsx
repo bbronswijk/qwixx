bec4591ce118561ee9fc611080422c31626b0c95
bbronswijk <brambronswijk@gmail.com>
2024-08-18 18:13:52 +0200

    do not automatically submit form

--- src/app/join-existing/page.tsx
+++ src/app/join-existing/page.tsx
@@ -50,35 +50,15 @@ export default function Page() {
     router.push(`/${response.pin}/${response.variant}`);
   }
 
-  const handleChange = async (gamePin: string) => {
-    if (gamePin.length !== 4) {
-      return
-    }
-
-    const response = await joinGameAction(Number(gamePin), nickname as string);
-
-    if ('error' in response) {
-      toast({
-        title: response.error.title,
-        description: response.error.description,
-        variant: 'destructive'
-      });
-      return;
-    }
-
-    reset();
-    router.push(`/${response.pin}/${response.variant}`);
-  }
-
   return (
-    <main className="h-full w-full flex flex-col p-4 justify-center items-center bg-slate-100 p-4">
+    <main className="h-full w-full flex flex-col p-4 justify-center items-center bg-slate-100">
       <header className="fixed top-3 left-3 right-3 gap-3 flex items-center justify-between">
         <BackButton/>
       </header>
       <form onSubmit={handleSubmit} className="border rounded-2xl p-8 bg-white w-full max-w-96 space-y-6">
         <h1 className="text-2xl text-center">Join an existing game</h1>
         <div className="flex justify-center flex-wrap">
-          <InputOTP ref={ref} maxLength={4} onChange={handleChange}>
+          <InputOTP ref={ref} maxLength={4}>
             <InputOTPGroup>
               <InputOTPSlot index={0}/>
               <InputOTPSlot index={1}/>
