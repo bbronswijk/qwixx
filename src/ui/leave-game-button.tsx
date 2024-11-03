36adc1fb0456718f4166182fb66aeb410384f5de
bbronswijk <brambronswijk@gmail.com>
2024-11-03 09:38:03 +0100

    Fix user not leaving the game if the user used the phones back button

--- src/ui/leave-game-button.tsx
+++ src/ui/leave-game-button.tsx
@@ -2,10 +2,25 @@ import { leaveGameAction } from "@/actions/game.actions";
 import { useAuth } from "@/auth/authentication.context";
 import { useGamePin } from "@/utils/use-game-pin.hook";
 import BackButton from "@/ui/back-button";
+import { useEffect, useRef } from "react";
+
+const isDevelopment = process.env.NODE_ENV === "development";
 
 export const LeaveGameButton = () => {
   const { nickname } = useAuth();
   const pin = useGamePin();
+  const mounted = useRef(false);
 
-  return <BackButton onClick={() => leaveGameAction(pin, nickname as string)} />;
+  useEffect(() => {
+    return () => {
+      // Prevent the leave game action from being called on the first render in development mode
+      if (isDevelopment && !mounted.current) {
+        mounted.current = true;
+        return;
+      }
+      leaveGameAction(pin, nickname as string);
+    };
+  }, []);
+
+  return <BackButton />;
 };
