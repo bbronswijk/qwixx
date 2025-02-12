a8c4208c8de19a06bbcf3a26bd5ff48dc23115f6
bbronswijk <bbronswijk@anwb.nl>
2025-02-12 21:48:41 +0100

    Use one of the 5 pre-configured configurations

--- src/app/[gameId]/steps/generate-configuration.utils.ts
+++ src/app/[gameId]/steps/generate-configuration.utils.ts
@@ -8,68 +8,20 @@ import { tileType } from "@/data/tile.model";
  * - There should be 2 - 4 steps per row.
  * - 2 steps should not be adjacent to each other.
  */
-export const generateConfiguration = (): Config => {
+export const generateConfiguration = (index: number): Config => {
   const baseConfig: Config = structuredClone(stepsTiles);
+  console.log(index, index % config.length);
 
-  const [a, b, c, d] = getNumberOfStepsPerRow();
+  config.at(index % config.length)!.forEach((row, index) => (baseConfig[row][index].type = tileType.step));
 
-  pickRandomItemsFromList(a).forEach((index) => (baseConfig[rows.a][index].type = tileType.step));
-  pickRandomItemsFromList(b).forEach((index) => (baseConfig[rows.b][index].type = tileType.step));
-  pickRandomItemsFromList(c).forEach((index) => (baseConfig[rows.c][index].type = tileType.step));
-  pickRandomItemsFromList(d).forEach((index) => (baseConfig[rows.d][index].type = tileType.step));
-
+  console.log(baseConfig);
   return baseConfig;
 };
 
-/**
- * Pick the amount of items from the list. 2 picks should not be adjacent to each other.
- */
-export const pickRandomItemsFromList = (amount: number) => {
-  const results: number[] = [];
-  let source = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const configA = [rows.b, rows.c, rows.d, rows.c, rows.b, rows.a, rows.b, rows.c, rows.d, rows.c, rows.b] as const;
+const configB = [rows.d, rows.c, rows.b, rows.a, rows.b, rows.c, rows.d, rows.c, rows.b, rows.a, rows.b] as const;
+const configC = [rows.c, rows.d, rows.c, rows.b, rows.a, rows.b, rows.c, rows.d, rows.c, rows.b, rows.a] as const;
+const configD = [rows.b, rows.a, rows.b, rows.c, rows.d, rows.c, rows.b, rows.a, rows.b, rows.c, rows.d] as const;
+const configE = [rows.a, rows.b, rows.c, rows.d, rows.c, rows.b, rows.a, rows.b, rows.c, rows.d, rows.c] as const;
 
-  for (let i = 0; i < amount; i++) {
-    const randomIndex = Math.floor(Math.random() * source.length);
-    const [picked] = source.splice(randomIndex, 1);
-
-    source = source.filter((item) => item !== picked - 1 && item !== picked + 1);
-
-    results.push(picked);
-  }
-
-  return results;
-};
-
-/**
- * Returns a random array of 4 numbers between 2-4. The total amount of numbers should be 11.
- */
-export const getNumberOfStepsPerRow = (): [number, number, number, number] => {
-  const minimumPerTile = 2;
-
-  // Pick a random number between 2-4 for the first two tiles.
-  const a: number = getRandomBetween(minimumPerTile, 4);
-
-  // max 2nd tiles =  previous - 2 * minimum of last 2 tiles.
-  // sum previous 2: max -> 4
-  // sum previous 3: max -> 4
-  // sum previous 4: max -> 3
-  const b: number = getRandomBetween(minimumPerTile, 11 - a - minimumPerTile - minimumPerTile);
-
-  // Max 3rd tiles = sum of previous 2 - minimum of last tile.
-  const tilesLeft: number = 11 - a - b; // Possibly sum of first 2 tiles is between 4 and 7
-  // 7 left: min -> 3, max -> 4
-  // 6 left: min -> 2, max -> 4
-  // 5 left: min -> 2, max -> 3
-  // 4 left: min -> 2, max -> 2
-  const c: number = getRandomBetween(Math.max(minimumPerTile, tilesLeft - 4), tilesLeft - minimumPerTile);
-  const d: number = 11 - a - b - c;
-
-  return [a, b, c, d];
-};
-
-/**
- * Returns a random integer between min and max, but never higher than 4.
- */
-const getRandomBetween = (min: number, max: number): number => {
-  return Math.min(Math.floor(Math.random() * (max - min + 1)) + min, 4);
-};
+const config = [configA, configB, configC, configD, configE];
--- src/app/[gameId]/steps/page.tsx
+++ src/app/[gameId]/steps/page.tsx
@@ -1,6 +1,17 @@
+"use client";
+
 import type { Viewport } from "next";
 import { generateConfiguration } from "@/app/[gameId]/steps/generate-configuration.utils";
-import { Client } from "@/app/[gameId]/steps/client";
+import { useTotalSelector } from "@/state/selectors";
+import { Variant, VariantContext } from "@/context/variant.context";
+import { TotalScoreContext } from "@/context/total-score.context";
+import { Pusher, usePusher } from "@/pusher/pusher.context";
+import { GameHeader } from "@/ui/game-header";
+import { Members } from "@/ui/members";
+import Board from "@/ui/board";
+import React from "react";
+import { Config } from "@/data/config.model";
+import { useAuth } from "@/auth/authentication.context";
 
 export const viewport: Viewport = {
   themeColor: "black",
@@ -11,5 +22,32 @@ export const viewport: Viewport = {
 };
 
 export default function Page() {
-  return <Client config={generateConfiguration()} />;
+  return (
+    <VariantContext.Provider value={Variant.STEPS}>
+      <Pusher>
+        <WithConfig />
+      </Pusher>
+    </VariantContext.Provider>
+  );
 }
+
+const WithConfig = () => {
+  const { nickname } = useAuth();
+  const { members } = usePusher();
+  const names = members.map((member) => member.nickname).toSorted();
+  return <Game config={generateConfiguration(names.indexOf(nickname))} />;
+};
+
+const Game = ({ config }: { config: Config }) => {
+  const totalScore = useTotalSelector(config);
+
+  return (
+    <TotalScoreContext.Provider value={totalScore}>
+      <main className='grid h-full w-full grid-cols-[1fr_auto_1fr] items-center justify-center p-4'>
+        <GameHeader />
+        <Members />
+        <Board config={config} />
+      </main>
+    </TotalScoreContext.Provider>
+  );
+};
