abf65b845ce399f75670b0c1878f2e70293a8e1a
bbronswijk <bbronswijk@anwb.nl>
2025-12-27 21:27:36 +0100

    Use @ducanh2912/next-pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 import { withSentryConfig } from "@sentry/nextjs";
-import withPWA from "next-pwa";
+import withPWA from "@ducanh2912/next-pwa";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
